c130923440a00e6805758ce1988035b32ae44b65
elyes <elyesbarnat42@gmail.com>
2017-04-23 18:41:21 +0200

    optimisation pour plusieurs niveaux

--- init.h
+++ init.h
@@ -3,7 +3,111 @@
 #define NB_BLOCS_HAUTEUR    12
 #define LARGEUR_FENETRE     TAILLE_BLOC * NB_BLOCS_LARGEUR
 #define HAUTEUR_FENETRE     TAILLE_BLOC * NB_BLOCS_HAUTEUR
-enum {VIDE, MUR, CAISSE, OBJECTIF, MARIO, CAISSE_OK}; // VIDE = 0, MUR = 1, CAISSE = 2 etc..
+enum {VIDE, MUR, CAISSE, OBJECTIF, MARIO}; // VIDE = 0, MUR = 1, CAISSE = 2 etc..
 void placerMur(SDL_Surface * fenetre, SDL_Surface * mur, SDL_Rect positionMur, int carte[NB_BLOCS_LARGEUR][NB_BLOCS_HAUTEUR]);
-void chargerImg(SDL_Surface * surf, SDL_Surface * img);
 void deplacerMario(SDL_Surface * mario, SDL_Surface * caisse, SDL_Rect * positionMario, SDL_Rect * positionCaisse, SDL_Surface * img, int carte[NB_BLOCS_LARGEUR][NB_BLOCS_HAUTEUR], int x, int y, char * direction);
+void chargerImg(SDL_Surface * surf, SDL_Surface * img) {
+	*surf = *img;
+}
+void initTab(int carte[NB_BLOCS_LARGEUR][NB_BLOCS_HAUTEUR]) {
+	int i, j;
+	for (i = 0; i < 12; i++) { 
+		for (j = 0; j < 12; j++){ 
+				carte[i][j] = MUR;
+		}
+	}
+}
+void niveau2(int carte[NB_BLOCS_LARGEUR][NB_BLOCS_HAUTEUR]) {
+	initTab(carte);
+	int i;	
+	carte[3][3] = MARIO;
+	for (i = 4; i < 8; i++)
+		carte[i][3] = VIDE;
+	carte[6][4] = CAISSE;
+	carte[7][4] = CAISSE;
+	carte[7][5] = VIDE;
+	carte[7][6] = OBJECTIF;
+	for (i = 5; i < 9; i++)
+		carte[6][i] = VIDE;
+	carte[6][9] = OBJECTIF;
+}
+void niveau3(int carte[NB_BLOCS_LARGEUR][NB_BLOCS_HAUTEUR]) {
+	initTab(carte);
+	int i;	
+	carte[2][4] = MARIO;
+	carte[3][4] = CAISSE;
+	for (i = 4; i < 8; i++)
+		carte[i][4] = VIDE;
+	for (i = 3; i < 9; i++) {
+		carte[6][i] = VIDE;
+		carte[7][i] = VIDE;
+	}
+	carte[8][7] = VIDE;
+	for (i = 2; i < 9; i++)
+		carte[i][8] = VIDE;
+	for (i = 7; i < 10; i++) {
+		carte[2][i] = VIDE;
+		if (i != 9)
+			carte[3][i] = VIDE;
+	}
+	carte[2][10] = OBJECTIF;
+}
+void majMap(SDL_Surface * fenetre, SDL_Surface * mario, SDL_Surface * caisse, SDL_Surface * mur, SDL_Surface * objectif, SDL_Rect * positionMario, SDL_Rect * positionCaisse, int carte[NB_BLOCS_LARGEUR][NB_BLOCS_HAUTEUR]) {
+	SDL_FillRect(fenetre, NULL, SDL_MapRGB(fenetre->format, 255, 255, 255));
+	SDL_Rect position;
+	int i, j;
+	for (i = 0; i < 12; i++)
+	{
+		for (j = 0; j < 12; j++)
+		{
+			position.x = i * TAILLE_BLOC;
+			position.y = j * TAILLE_BLOC;
+			if(carte[i][j] == MUR) 
+				SDL_BlitSurface(mur, NULL, fenetre, &position);
+			else if(carte[i][j] == OBJECTIF)
+				SDL_BlitSurface(objectif, NULL, fenetre, &position);
+			else if(carte[i][j] == MARIO)		
+				SDL_BlitSurface(mario, NULL, fenetre, positionMario);
+			else if(carte[i][j] == CAISSE)		
+				SDL_BlitSurface(caisse, NULL, fenetre, positionCaisse);
+		}
+	}
+	SDL_Flip(fenetre);
+}
+void deplacement(SDL_Surface * fenetre, SDL_Surface * mario, SDL_Rect * positionMario, SDL_Rect * positionCaisse, int carte[NB_BLOCS_LARGEUR][NB_BLOCS_HAUTEUR], char direction) { // 'u' = up, 'd' = down, 'l' = left et 'r' = right.
+	switch(direction) {
+		case 'u' :
+			chargerImg(mario, IMG_Load("mario/mario_haut.gif"));
+			if (carte[positionMario->x/34][ positionMario->y/34 - 1] != MUR) {
+				carte[positionMario->x/34][positionMario->y/34] = VIDE;
+				positionMario->y-=34;
+				carte[positionMario->x/34][positionMario->y/34] = MARIO;
+			}
+			break;
+		case 'd' :
+			chargerImg(mario, IMG_Load("mario/mario_bas.gif"));
+			if (carte[positionMario->x/34][positionMario->y/34 +1] == VIDE) {
+				carte[positionMario->x/34][positionMario->y/34] = VIDE;
+				positionMario->y+=34;
+				carte[positionMario->x/34][positionMario->y/34] = MARIO;
+			}
+			break;
+		case 'l' :
+			chargerImg(mario, IMG_Load("mario/mario_gauche.gif"));
+			printf("%d %d %d\n", carte[positionMario->x /34 - 1][ positionMario->y/34], positionMario->x/34, positionMario->y/34);
+			if (carte[positionMario->x /34 - 1][positionMario->y/34] == VIDE) {
+				carte[positionMario->x/34][positionMario->y/34] = VIDE;
+				positionMario->x-=34;
+				carte[positionMario->x/34][positionMario->y/34] = MARIO;
+			}
+			break;
+		case 'r' :
+			chargerImg(mario, IMG_Load("mario/mario_droite.gif"));
+			if (carte[positionMario->x /34 + 1][positionMario->y/34] == VIDE) {
+				carte[positionMario->x/34][positionMario->y/34] = VIDE;
+				positionMario->x+=34;
+				carte[positionMario->x/34][positionMario->y/34] = MARIO;
+			}
+			break;
+	}
+}
---
+++ run.c
@@ -0,0 +1,88 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include "init.h"
+#include <SDL_ttf.h>
+int main(int argc, char const *argv[])
+{
+	SDL_Init(SDL_INIT_VIDEO);
+	// Declaration des surfaces
+	SDL_Surface * fenetre = NULL, * mario = NULL, * mur = NULL,* caisse = NULL,* objectif = NULL;
+
+	fenetre = SDL_SetVideoMode(LARGEUR_FENETRE, HAUTEUR_FENETRE, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
+	mario = IMG_Load("mario/mario_bas.gif");
+	mur = IMG_Load("mario/mur.jpg");
+	caisse = IMG_Load("mario/caisse.jpg");
+	objectif = IMG_Load("mario/objectif.png");
+
+	// Modif couleur background
+	SDL_FillRect(fenetre, NULL, SDL_MapRGB(fenetre->format, 255, 255, 255));
+
+	// Map du jeu
+	int carte[NB_BLOCS_LARGEUR][NB_BLOCS_HAUTEUR] = {0};
+	niveau3(carte);
+	
+	int continuer = 1;
+	SDL_Flip(fenetre);
+
+	SDL_Rect positionMario, positionCaisse;
+	int i, j;
+	for (i = 0; i < 12; i++) { 
+		for (j = 0; j < 12; j++){ 
+				if (carte[i][j] == MARIO) {
+					positionMario.x = i*TAILLE_BLOC;
+					positionMario.y = j*TAILLE_BLOC;
+				}
+				else if(carte[i][j] == CAISSE) {
+					positionCaisse.x = i*TAILLE_BLOC;
+					positionCaisse.y = j*TAILLE_BLOC;
+				}
+		}
+	}
+	// Activation de la repetition des touches
+	SDL_EnableKeyRepeat(100, 100);
+	// Boucle principal
+	while(continuer) {
+		SDL_Event event;
+		// printf("%d %d\n", event.motion.x, event.motion.y);
+		SDL_WaitEvent(&event);
+		switch(event.type) {
+			case SDL_QUIT :
+				continuer = 0;
+			break;
+			case SDL_KEYDOWN :
+				switch(event.key.keysym.sym)
+                {
+                    case SDLK_ESCAPE:
+                        continuer = 0; 
+                        break;
+                    case SDLK_UP:
+                    	deplacement(fenetre, mario, &positionMario, &positionCaisse, carte, 'u');
+                        break;
+                    case SDLK_DOWN:
+                    	deplacement(fenetre, mario, &positionMario, &positionCaisse, carte, 'd');
+                        break;
+                    case SDLK_RIGHT:
+                    	deplacement(fenetre, mario, &positionMario, &positionCaisse, carte, 'r');
+                        break;
+                    case SDLK_LEFT:
+                    	deplacement(fenetre, mario, &positionMario, &positionCaisse, carte, 'l');
+                        break;
+                }
+            break;
+		}
+		majMap(fenetre, mario, caisse, mur, objectif, &positionMario, &positionCaisse, carte);
+		SDL_Flip(fenetre);
+	}
+	SDL_EnableKeyRepeat(0, 0);
+    // Libération des surfaces chargées
+    SDL_FreeSurface(mario);
+    SDL_FreeSurface(mur);
+    SDL_FreeSurface(caisse);
+    SDL_FreeSurface(objectif);
+    SDL_FreeSurface(fenetre);
+	SDL_Quit();
+	return 0;
+}
+
